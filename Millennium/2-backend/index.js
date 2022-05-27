146ff0b8d62209c7136b8e92dbe1d47bcc4d5368
gabriosilva <gabrio.lina@hotmail.com>
2022-05-26 21:20:26 -0300

    refact(endpoints): fix invalid table names and remove unused code

--- Millennium/2-backend/index.js
+++ Millennium/2-backend/index.js
@@ -17,34 +17,6 @@ app.use(express.static("../1-frontend/"));
 // use json as middleware
 app.use(express.json());
 
-// endpoints
-
-app.get("/agendas", (request, response) => {
-  let db = new sqlite3.Database(DBPATH);
-  let sql = "SELECT * FROM Agenda";
-  db.all(sql, [], (err, rows) => {
-    response.statusCode = 200;
-    response.json(rows);
-  });
-  db.close();
-});
-
-app.post("/redes/create", (request, response) => {
-  let db = new sqlite3.Database(DBPATH);
-  let sql = "INSERT INTO Rede (nome, email, chaveAcesso) VALUES(?, ?, ?)";
-  console.log(request.body);
-  let params = [];
-  params.push(request.body.nome);
-  params.push(request.body.email);
-  params.push(request.body.chaveAcesso);
-
-  db.all(sql, params, (err, rows) => {
-    response.statusCode = 200;
-    response.json(rows);
-  });
-  db.close();
-});
-
 // ENDPOINTS API USUÁRIO
 
 // api usuário escola
@@ -68,7 +40,7 @@ app.get("/usuarioRede/:AccountId", (request, response) => {
   let db = new sqlite3.Database(DBPATH);
   let params = [];
   params.push(request.params.AccountId);
-  let sql = "SELECT * FROM Account WHERE id=?";
+  let sql = "SELECT * FROM Rede WHERE id=?";
 
   db.all(sql, params, (err, rows) => {
     response.statusCode = 200;
@@ -83,7 +55,7 @@ app.get("/usuarioFalconi/:AccountId", (request, response) => {
   let db = new sqlite3.Database(DBPATH);
   let params = [];
   params.push(request.params.AccountId);
-  let sql = "SELECT * FROM Account WHERE id=?";
+  let sql = "SELECT * FROM AdminFalconi WHERE id=?";
 
   db.all(sql, params, (err, rows) => {
     response.statusCode = 200;
@@ -91,26 +63,6 @@ app.get("/usuarioFalconi/:AccountId", (request, response) => {
   });
   db.close();
 });
-
-// // returns the workExperience list
-// app.get("/workExperience", (req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-
-//   let db = new sqlite3.Database(DBPATH);
-//   let sql = "SELECT * FROM workExperience ORDER BY startDate";
-//   let params = [];
-
-//   db.all(sql, params, (err, rows) => {
-//     if (err) {
-//       throw err;
-//     }
-
-//     // response
-//     res.json({ workExperiences: rows });
-//   });
-//   db.close();
-// });
 
 // starts the server
 app.listen(port, hostname, () => {
