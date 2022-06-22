0d6cdb8d75d82e0eae358c47081e61b9110e777e
emanuel-inteli <82065728+emanuelcop3@users.noreply.github.com>
2022-06-22 14:02:21 -0300

    criação delete

--- src/2-backend/controllers/dominiosController.js
+++ src/2-backend/controllers/dominiosController.js
@@ -1,4 +1,5 @@
-const { createNewDominio } = require("../models/dominiosModel");
+const { request } = require("http");
+const { createNewDominio, deleteNewDominio } = require("../models/dominiosModel");
 
 exports.postCreateDominio = async (request, response) => {
   response.setHeader("Access-Control-Allow-Origin", "*");
@@ -14,3 +15,20 @@ exports.postCreateDominio = async (request, response) => {
   response.statusCode = responseMessage.code;
   response.json(responseMessage);
 };
+
+
+exports.postDeleteDominio = async (request, response) => {
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  const responseMessage = { message: "success", code: 200, respostas: {} };
+  try {
+    console.log(request.body.id)
+    await deleteNewDominio(request.body.id);
+  } catch (err) {
+    console.log(133333);
+    responseMessage.code = 500;
+    responseMessage.message = err.message;
+    responseMessage.trace = err.stack;
+  }
+  response.statusCode = responseMessage.code;
+  response.json(responseMessage);
+}
